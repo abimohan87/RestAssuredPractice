8c1ad91a5a64acb57696cece921da1c5a31a1624
Abishek Mohan <abimohan200587@gmail.com>
2022-01-15 18:52:33 +0530

    Second Commit

---
+++ src/test/java/restAssuredSample/JSONParsing.java
@@ -0,0 +1,11 @@
+package restAssuredSample;
+
+import data.Payload;
+
+public class JSONParsing {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Payload.dummyPayload());
+	}
+}
